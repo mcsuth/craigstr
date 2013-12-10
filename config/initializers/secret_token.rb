65f228d1b4a468e7a9160126a937e5c84be50502
Seimith <seimith@gmail.com>
2013-12-09 16:56:37 -0800

    First Commit - Empty Rails Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Craigstr::Application.config.secret_token = 'deef62a316a19cd9b1e284ae474b09f2f48410148534b04f49c5e3e7eceda23427457aaacfac9708f3cf10d6fdf80c3247cd09535376fbd4709399c228273031'
