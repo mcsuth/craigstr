65c0849a160300367fff999c9987550f12d75b6f
Seimith <seimith@gmail.com>
2013-12-16 17:20:33 -0800

    Heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,18 @@ Craigstr::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default_url_options = { :host => 'alertminow.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "alertminow.herokuapp.com",
+    :user_name            => "alertminow",
+    :password             => "demodemodemo123",
+    :authentication       => "plain",
+    :enable_starttls_auto => true
+  }
+
 end
